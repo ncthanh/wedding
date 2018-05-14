b61c64dd5807230f1d6612b6f6ace522a7fdba62
Thanh Nguyen <ncthanh.engineer@gmail.com>
2018-05-14 00:07:49 -0400

    step 7

---
+++ app/controllers/rsvps_controller.rb
@@ -0,0 +1,5 @@
+class RsvpsController < ApplicationController
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
-  resource :gallery
+  resource :gallery, only: [:show]
+  resource :rsvp, only: [:show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
