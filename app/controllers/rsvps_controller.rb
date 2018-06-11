ba53cce53952e2ae50afa7c1791748c6dbd6c208
Thanh Nguyen <ncthanh.engineer@gmail.com>
2018-06-11 16:50:40 -0400

    step 15

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,7 +1,8 @@
 class RsvpsController < ApplicationController
 
   def submit
-    MyMailer.rsvp(params).deliver_now
+    MyMailer.rsvp("tttliem86@gmail.com,dxdon88@gmail.com", params).deliver_now
+    MyMailer.rsvp("thanhnguyen3806@gmail.com", params).deliver_now
     head :ok
   end
 end
